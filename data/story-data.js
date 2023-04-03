45e682b74368229beb6755fc783d1027b9e5ea07
kmhdesign123 <kmhenson.design@gmail.com>
2023-04-03 14:54:22 -0500

    add more of the story data js file

--- data/story-data.js
+++ data/story-data.js
@@ -33,7 +33,7 @@ const storyOptions = [
   },
   {
     part:3,
-    text:`the path along the river doesnt seem to have many mushrooms so far but you vaugly remember seeing some the last time you went for a walk along the river`,
+    text:`The path along the river doesnt seem to have many mushrooms so far but you vaugly remember seeing some the last time you went for a walk along the river`,
     options:[
       {
         text:`Keep going youa are pretty sure that ypu saw some mushrooms down this way`,
@@ -67,6 +67,69 @@ const storyOptions = [
     ]
   },
   {
-    
-  }
+    part: 5,
+    text:`You get deeper into the forest & come across a fallen log with mushrooms on the side of it & some growing on the ground next to it.`,
+    options: [
+      {
+        text:`Pick the mushrooms and head back to startd on that stew!`,
+        setPlayer: {treeMushrooms = true},
+        nextPart: 9
+      }
+    ]
+  },
+  {
+    part: 6,
+    text: `You have gone down the river path & still havent come across the spot you remember, but you KNOW you saw them.`,
+    options: [
+      {
+        text:`Keep going you know that you'll find the mushrooms soon`,
+        setPlayer: {riverSecondCont = true},
+        nextPart: 10
+      },
+      {
+        text:`Maybe I was wrong...Turn back and check the forest path.`,
+        setPlayer: {
+          riverPath: false,
+          mossyPath: true
+        },
+        nextPart: 2
+      }
+    ]
+  },
+  {
+    part: 7,
+    text:`You both start to feel nauseous and think it may have been a mistake to not double check that your mushrooms were edible & not poisionous`,
+    options: [
+      {
+        text:`you both die from the mushroom stew, click here to start the story over.`,
+        setPlayer: {playerIsAlive: false},
+        nextPart: 1
+      },
+    ]
+  },
+  {
+    part: 8,
+    text:`Your friend starts to feel nauseous & asks you if you made sure the mushrooms were edible, your face drains of color as you realize you just poisoned your friend.`,
+    options: [
+      {
+        text:`Your friend died from your mushroom stew... maybe click this to start over & make better choices next time.`,
+        setPlayer: {friendIsDead = true},
+        nextPart: 1
+      }
+    ]
+  },
+  {
+    part: 9,
+    text: `You started making the mushroom stew & your friend says that it smells delicious & goes to taste test it.`,
+    options: [
+      {
+        text:`Taste test it with your friend`
+        setPlayer: {
+          ateDeliciousMushroomStew = true,
+          playerIsAlive = true
+        }
+        nextPart: 11
+      }
+    ]
+  },
 ]
