df8954f80841af4f8cd719ff5d14d1335bdbfc20
kmhdesign123 <kmhenson.design@gmail.com>
2023-04-04 09:41:01 -0500

    add button display functionality

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let player = {}
 /*------------------------ Cached Element References ------------------------*/
 
 const storyEl = document.getElementById(`story-text`)
-const optionBtnEl =document.getElementById(`option-btns`)
+const optionBtnEl = document.getElementById(`option-btns`)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -22,8 +22,25 @@ showStoryText(1)
 }
 
 function showStoryText(storyOptionIndex) {
- const storyOption = storyOptions.find(storyOption => storyOption.part === storyOptionIndex)
- storyEl.innerText = storyOption.text
+  const storyOption = storyOptions.find(storyOption => storyOption.part === storyOptionIndex)
+  storyEl.innerText = storyOption.text
+  while (optionBtnEl.firstChild) {
+  optionBtnEl.removeChild(optionBtnEl.firstChild)
+  }
+  storyOption.options.forEach(option => {
+    if (showOption(option)) {
+    const button = document.createElement(`button`)
+    button.innerText = option.text
+    button.classList.add('btn')
+    button.addEventListener(`click`, () => selectOption(option))
+    optionBtnEl.appendChild(button)
+    console.log(option)
+    }
+  })
+}
+
+function showOption(option) {
+  return true
 }
 
 function selectOption(option) {
